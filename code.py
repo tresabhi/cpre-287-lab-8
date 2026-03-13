c649d4a7a5b1a878b2c055ce1971c32271187bdf
tresabhi <43380238+tresabhi@users.noreply.github.com>
2026-03-13 09:38:28 -0500

    relay test with print debug

--- code.py
+++ code.py
@@ -11,4 +11,7 @@ led_red.direction = digitalio.Direction.OUTPUT
 while True:
     led_red.value = led_blue.value
     led_blue.value = not led_blue.value
+
+    print("red" if led_red.value else "blue")
+
     time.sleep(1)
